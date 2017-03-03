b35dfd7ef250ee2bc655ff9f3d405a2299da0b85
weathare <fuzuki.close@gmail.com>
2017-03-03 18:10:29 +0900

    add request body parser

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var server     = jsonServer.create();
 var router     = jsonServer.router('db.json');
 var middleware = jsonServer.defaults();
 var auth       = require('basic-auth');
-var express    = require("express");
+var bodyParser = require('body-parser');
+var logger     = require('morgan-body');
 
 server.use(middleware);
+server.use(bodyParser.json());
+logger(server);
 server.use(function (req, res, next) {
   var check = auth(req);
   if (!check || check.name !== 'john' || check.pass !== 'qwertyuiop@') {
@@ -14,9 +17,9 @@ server.use(function (req, res, next) {
  } else {
     next();
   }
-})
+});
 
 server.use(router);
 server.listen(3000, function() {
   console.log("JSON Server is running");
-})
+});
